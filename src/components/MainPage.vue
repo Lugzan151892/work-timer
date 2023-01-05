a82323fb6662838047f521774f910b96162ae88e
Denis Gukov <lgzn151892@gmail.com>
2023-01-05 17:06:34 +0300

    fix: fixed icons on main page

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -3,16 +3,19 @@
     <q-header bordered class="header bg-primary text-white">
       <q-toolbar>
         <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
-        <q-toolbar-title>
+        <q-toolbar-title class="header-container">
           <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+            <img src="../assets/icons8-time-machine-100.png">
           </q-avatar>
-          Work-learn Timer
+          <p class="text">
+            Work-learn Timer
+          </p>
         </q-toolbar-title>
       </q-toolbar>
     </q-header>
-    <q-drawer v-model="leftDrawerOpen" side="left" overlay elevated>
+    <q-drawer align="center" v-model="leftDrawerOpen" side="left" overlay elevated>
       <!-- drawer content -->
+      В разработке авторизация и личная статистика
     </q-drawer>
     <q-page-container>
       <slot></slot>
@@ -32,5 +35,12 @@ const toggleLeftDrawer = () => {
 <style scoped>
 .header {
   background-color: #26A69A !important;
+}
+.header-container {
+  display: flex;
+  align-items: center;
+}
+.text {
+  margin: 0 0 0 10px;
 }
 </style>
